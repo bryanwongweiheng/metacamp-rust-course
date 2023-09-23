7756b848c8b54258ff91483fe3bf4980b1179032
bryanwongweiheng <bryanwongweiheng@gmail.com>
2023-09-23 19:43:47 +0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-#[derive(Debug)]
-struct A<T>(T);
-
 fn main() {
-    let a = A(0);
-    println!("{:?}", a);
+    println!("Hello, world!");
 }
--- src/transaction.rs
+++
@@ -1,9 +0,0 @@
-pub fn withdrawal() {
-println!("User requested a withdrawal")
-}
-pub fn deposit() {
-println!("User requested a deposit")
-}
-pub fn transfer() {
-println!("User requested a transfer")
-}
